1e18a317a48a9550f8c4fda93a32ca9d959c5f48
PatrickJS <github@patrickjs.com>
2023-11-14 19:07:15 -0800

    refactor: move around

--- src/routes/(csr)/user/[user]/index.tsx
+++
@@ -1,9 +0,0 @@
-import { component$ } from '@builder.io/qwik';
-import { useLocation } from '@builder.io/qwik-city';
-
-import UserShell from "~/routes/ssg/user/[user]/index";
-
-export default component$(() => {
-  const route = useLocation();
-  return <UserShell>{route.params.user}</UserShell>;
-});
